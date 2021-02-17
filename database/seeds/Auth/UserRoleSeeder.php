d7725c415d2738566d0211cf5a6f7d7c6e7918c0
ianvizarra <ian.vizarra@gmail.com>
2021-02-17 21:11:19 +0800

    fix: role seeder

--- database/seeds/Auth/UserRoleSeeder.php
+++ database/seeds/Auth/UserRoleSeeder.php
@@ -24,10 +24,11 @@ class UserRoleSeeder extends Seeder
 
         $operator = Company::firstOrCreate(['name' => 'Operator']);
 
-        $masterAgent = factory(User::class)->state('master_agent')->create(['email' => 'master@agent.com']);
-        $masterAgent = factory(User::class)->state('master_agent')->create(['email' => 'master@agent2.com']);
+        $masterAgent = factory(User::class)->state('master_agent')->create(['email' => 'master@agent.com',  'referral_id' => 'master1']);
+        $masterAgent2 = factory(User::class)->state('master_agent')->create(['email' => 'master@agent2.com', 'referral_id' => 'master2']);
         $betAdmin = factory(User::class)->state('bet_admin')->create(['email' => 'bet@admin.com']);
         $player = factory(User::class)->state('player')->create(['email' => 'dropball@player.com', 'referred_by' => $masterAgent->id]);
+        $player2 = factory(User::class)->state('player')->create(['email' => 'dropball2@player.com', 'referred_by' => $masterAgent->id]);
         $virtualHub = factory(User::class)->state('virtual-hub')->create(['email' => 'admin@hub.com']);
         $satoshi = factory(User::class)->state('satoshi')->create(['email' => 'satoshi@nakamoto.com']);
         $operator = factory(User::class)->state('operator')->create(['email' => 'operator@admin.com']);
