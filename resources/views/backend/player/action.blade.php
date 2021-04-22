92c45e9600e7e59e0d88d918558f38422a683630
ianvizarra <ian.vizarra@gmail.com>
2021-04-23 00:51:28 +0800

    fix: verified players

--- resources/views/backend/player/action.blade.php
+++ resources/views/backend/player/action.blade.php
@@ -1,5 +1,7 @@
-<a href='{{route('admin.players.info',$user)}}' class='btn btn-sm btn-primary text-white'><i class='fa fa-eye'></i> View Player</a>
-<a href='{{route('admin.players.wallet',$user)}}' class='btn btn-sm btn-info text-white'><i class='fa fa-dollar-sign'></i> Wallet</a>
+@if($user->isVerified())
+    <a href='{{route('admin.players.info',$user)}}' class='btn btn-sm btn-primary text-white'><i class='fa fa-eye'></i> View Player</a>
+    <a href='{{route('admin.players.wallet',$user)}}' class='btn btn-sm btn-info text-white'><i class='fa fa-dollar-sign'></i> Wallet</a>
+@endif
 @if(!$user->isVerified())
     <x-utils.form-button :action="route('admin.players.verify', $user)" button-class="btn btn-success btn-sm">Verify </x-utils.form-button>
 @endif
