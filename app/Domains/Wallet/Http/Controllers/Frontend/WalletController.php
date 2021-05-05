27d3d5e0648af9e55e2f32b0bf7704b8ff2ff275
ianvizarra <ian.vizarra@gmail.com>
2021-05-06 00:20:59 +0800

    fix: wallet controller for withdrawal request

--- app/Domains/Wallet/Http/Controllers/Frontend/WalletController.php
+++ app/Domains/Wallet/Http/Controllers/Frontend/WalletController.php
@@ -6,6 +6,7 @@ namespace App\Domains\Wallet\Http\Controllers\Frontend;
 use App\Domains\Auth\Models\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\WithdrawalRequest;
+use Bavix\Wallet\Models\Transaction;
 use Illuminate\Http\Request;
 
 class WalletController extends Controller
@@ -13,6 +14,7 @@ class WalletController extends Controller
     public function index(Request $request)
     {
         $user = $request->user();
+
         return view('frontend.pages.wallet.index')
             ->with('user', $user);
     }
@@ -23,8 +25,9 @@ class WalletController extends Controller
         $user = $request->user();
 
         try {
-            $user->withdrawFloat($request->get('amount'), null, false);
-
+            /* @var Transaction $withdrawalTransaction */
+            $withdrawalTransaction = $user->withdrawFloat($request->get('amount'), null, false);
+            $user->transferFloat($user->masterAgent, $request->get('amount'), ['withdrawal' => true, 'transaction' => $withdrawalTransaction->uuid]);
             return redirect()->back()->withFlashSuccess("Withdrawal request of ". number_format($request->get('amount')). " submitted.");
         } catch (\Exception $e) {
             return redirect()->back()->withErrors("Insufficient funds. Your current balance is ". number_format($user->balance));
