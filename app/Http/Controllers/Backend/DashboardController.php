916b6036098d422baa7b4360824cde871d75ef65
JEROME PALIS <jerome02@simplexi.com>
2021-07-14 16:42:02 +0800

    added dealer's account

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -30,7 +30,9 @@ class DashboardController extends Controller
             return $this->masterAgent();
         } elseif ($user->hasRole('Bet Admin')) {
             return $this->betAdmin();
-        } elseif ($user->hasRole('Virtual Hub')) {
+        } elseif ($user->hasRole('Dealer Admin')) {
+            return $this->dealerAdmin();
+        }elseif ($user->hasRole('Virtual Hub')) {
             return $this->virtualHub();
         } elseif ($user->hasRole('Operator')) {
             return $this->operator();
@@ -57,6 +59,16 @@ class DashboardController extends Controller
         return view('backend.dashboard.bet-admin')
             ->with('user', $user)
             ->with('bettingEvent', $bettingEvent);
+    }
+    public function dealerAdmin()
+    {
+        /** @var User $user */
+        $user = auth()->user();
+        //$bettingEvent = BettingEvent::today($user->timezone)->where('dealer_admin_id', $user->id)->first() ?? null;
+
+        return view('backend.dashboard.dealer-admin')
+            ->with('user', $user);
+            //->with('bettingEvent', $bettingEvent);
     }
 
     public function superAdmin()
