61237c633a7ec34ebe1d619addbf3131d0654191
Jerome Salcedo Palis <jerome02@simplexi.com.ph>
2021-08-27 15:48:55 +0800

    fix: changed wallet button label to transactions label

--- resources/views/backend/hub/action.blade.php
+++ resources/views/backend/hub/action.blade.php
@@ -2,5 +2,5 @@
 <a href='{{route('admin.hubs.info',$hub)}}' class='btn btn-sm btn-primary text-white'><i class='fa fa-eye'></i> View Hub</a>
 <a href='{{route('admin.hubs.edit',$hub)}}' class='btn btn-sm btn-warning text-white'><i class='fa fa-edit'></i> Edit Hub</a>
 @endunlessrole
-<a href='{{route('admin.hubs.wallet',$hub)}}' class='btn btn-sm btn-info text-white'><i class='fa fa-dollar-sign'></i> Wallet</a>
+<a href='{{route('admin.hubs.wallet',$hub)}}' class='btn btn-sm btn-info text-white'><i class='fa fa-dollar-sign'></i> Transactions</a>
 
--- resources/views/backend/player/action.blade.php
+++ resources/views/backend/player/action.blade.php
@@ -1,6 +1,6 @@
 @if($user->isVerified())
     <a href='{{route('admin.players.info',$user)}}' class='btn btn-sm btn-primary text-white'><i class='fa fa-eye'></i> View Player</a>
-    <a href='{{route('admin.players.wallet',['sorts' => ['created_at' => 'desc'], 'player' => $user])}}' class='btn btn-sm btn-info text-white'><i class='fa fa-dollar-sign'></i> Wallet</a>
+    <a href='{{route('admin.players.wallet',['sorts' => ['created_at' => 'desc'], 'player' => $user])}}' class='btn btn-sm btn-info text-white'><i class='fa fa-dollar-sign'></i> Transactions</a>
     <a href='{{route('admin.players.bet-histories',['sorts' => ['created_at' => 'desc'], 'player' => $user])}}' class='btn btn-sm btn-warning text-white'><i class='fa fa-calendar'></i> Bet History</a>
 @endif
 @if(!$user->isVerified())
