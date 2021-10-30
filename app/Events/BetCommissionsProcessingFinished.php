44bddf4056ebfa73948af269a3455816829f9c0e
Jerome Palis <jershe05@gmail.com>
2021-10-30 21:33:21 +0800

    fix: shouldbroadcastnow

--- app/Events/BetCommissionsProcessingFinished.php
+++ app/Events/BetCommissionsProcessingFinished.php
@@ -6,12 +6,13 @@ use App\Domains\Bet\Models\Bet;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class BetCommissionsProcessingFinished implements ShouldBroadcast, ShouldQueue
+class BetCommissionsProcessingFinished implements ShouldBroadcastNow, ShouldQueue
 {
     use Dispatchable, InteractsWithSockets, SerializesModels, InteractsWithQueue;
 
--- app/Events/ConfirmBetBettingResult.php
+++ app/Events/ConfirmBetBettingResult.php
@@ -6,12 +6,13 @@ use App\Domains\BettingRound\Models\BettingRound;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class ConfirmBetBettingResult implements ShouldBroadcast, ShouldQueue
+class ConfirmBetBettingResult implements ShouldBroadcastNow, ShouldQueue
 {
     use Dispatchable, InteractsWithSockets, SerializesModels, InteractsWithQueue;
 
