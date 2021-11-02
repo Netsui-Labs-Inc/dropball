9b29907067e048c838e6d3acb5886059abae9df1
Jerome Palis <jershe05@gmail.com>
2021-11-02 19:30:14 +0800

    ip test

---
+++ app/Http/Livewire/CashIn.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Livewire\Component;
+
+class CashIn extends Component
+{
+    public $amount = 102;
+    public $referenceNumber = '123-2321';
+    public function createInvoice(Request $request)
+    {
+        dd($request->ip());
+        $invoice = Http::withBasicAuth(config('cash-in.CASH_IN_API_KEY'), '')
+            ->withBody(json_encode([
+                'transaction_reference_number' => $this->referenceNumber,
+                'amount' => $this->amount,
+                "cash_in" => "Invoice Testing"
+            ]), 'json')
+            ->post(config('cash-in.CASH_IN_URL'));
+            dd($invoice->json());
+    }
+
+    public function render()
+    {
+        return view('livewire.cash-in');
+    }
+}
