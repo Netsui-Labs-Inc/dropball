a6088ac4564e8065df50c6dfef306a0e775c8a99
Jerome Palis <jershe05@gmail.com>
2021-11-05 16:28:39 +0800

    feat: changed ably key for production deployment

--- resources/js/backend/app.js
+++ resources/js/backend/app.js
@@ -7,15 +7,15 @@ require('../bootstrap');
 require('../plugins');
 require('./eyecandy');
 require('./alert');
-require('argon-design-system-free/assets/js/argon-design-system.min.js');
+// require('argon-design-system-free/assets/js/argon-design-system.min.js');
 import Echo from 'laravel-echo';
 import AmendmentDetails from './amendment-details';
 
 window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    // key: 'e7c1lA.fsp_Aw', //production
-    key: '12n6rA.Ws5Q3Q', //staging
+    key: 'e7c1lA.fsp_Aw', //production
+    // key: '12n6rA.Ws5Q3Q', //staging
     wsHost: 'realtime-pusher.ably.io',
     wsPort: 443,
     disableStats: true,
